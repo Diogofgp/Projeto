f2c85fead2876aa912dfc513f1c1a2633fadd619
Diogo Pereira <diogo.pereira@impactzero.pt>
2022-04-26 18:59:36 +0100

    added total users number in home card

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,6 +6,7 @@ import { Project } from '../projects_component/project.model';
 import { ApiService } from 'src/app/services/api-service/api-service.service';
 import { Observable, of, Subscription } from 'rxjs';
 import { LoadingService } from 'src/app/services/loading';
+import { User } from '../users/users.model';
 
 @Component({
   selector: 'app-home',
@@ -19,7 +20,9 @@ export class HomeComponent implements OnInit, OnDestroy {
   /* @ViewChild('comp1') comp1: ProjectListComponent; */
 
   public projectList = [];
-  private subscription: Subscription;
+  public users = [];
+  private usersSub: Subscription;
+  private projectSub: Subscription;
   cards = new Observable;
 
   loading$ = this.loader.loading$;
@@ -28,36 +31,54 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
 
-    this.subscription = this.apiService.getProjects()
+    this.projectSub = this.apiService.getProjects()
       .subscribe(
-        (projectList: Project[]) => {
-          this.projectList = projectList;
-          console.log(this.projectList.length)
+        (proj: Project[]) => {
+          this.projectList = proj;
+          /*  console.log(this.projectList.length) */
 
-          this.cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-            map(({ matches }) => {
-              if (matches) {
-                //if resolution is lower, adjustments
-                return [
-                  { title: 'Total Projects', cont: this.projectList.length, cols: 1, rows: 1 },
-                  { title: 'Card 2', cont: 'bruh', cols: 1, rows: 1 },
-                  { title: 'Card 3', cont: 'imagina', cols: 1, rows: 1 },
-                  { title: 'Card 4', cont: 'ligma', cols: 1, rows: 1 }
-                ];
+          this.usersSub = this.apiService.getUsers()
+            .subscribe(
+              (u: User[]) => {
+                this.users = u;
+                console.log(this.users.length + "users length")
+
+
+                this.cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+                  map(({ matches }) => {
+                    if (matches) {
+                      //if resolution is lower, adjustments
+                      return [
+                        { title: 'Total Projects', cont: this.projectList.length, cols: 1, rows: 1 },
+                        { title: 'Total Users', cont: this.users.length, cols: 1, rows: 1 },
+                        { title: 'Card 3', cont: 'imagina', cols: 1, rows: 1 },
+                        { title: 'Card 4', cont: 'ligma', cols: 1, rows: 1 }
+                      ];
+                    }
+
+                    return [
+                      { title: 'Total Projects', cont: this.projectList.length, cols: 1, rows: 1 },
+                      { title: 'Total Users', cont: this.users.length, cols: 1, rows: 1 },
+                      { title: 'Card 3', cont: 'imagina', cols: 1, rows: 1 },
+                      { title: 'Card 4', cont: 'ligma', cols: 1, rows: 1 }
+                    ];
+                  })
+                );
               }
-
-              return [
-                { title: 'Total Projects', cont: this.projectList.length, cols: 1, rows: 1 },
-                { title: 'Card 2', cont: 'bruh', cols: 1, rows: 1 },
-                { title: 'Card 3', cont: 'imagina', cols: 1, rows: 1 },
-                { title: 'Card 4', cont: 'ligma', cols: 1, rows: 1 }
-              ];
-            })
-          );
-
+            );
         }
       );
   }
+
+  /* getTotalUsers() {
+    this.subscription = this.apiService.getUsers()
+      .subscribe(
+        (u: User[]) => {
+          this.users = u;
+        }
+      );
+    return this.users.length;
+  } */
 
 
 
@@ -117,7 +138,8 @@ export class HomeComponent implements OnInit, OnDestroy {
   ); */
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.usersSub.unsubscribe();
+    this.projectSub.unsubscribe();
   }
 
 
