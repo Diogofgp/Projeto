ea7abee2c65ab32bc658481fba5c34b019977953
AndreFFranco <andrefranco@ipvc.pt>
2022-05-04 15:22:50 +0100

    Time estimates

--- src/app/components/issues/issue-details/issue-details.component.ts
+++ src/app/components/issues/issue-details/issue-details.component.ts
@@ -20,7 +20,9 @@ export class IssueDetailsComponent implements OnInit {
   public issueList = <any>[];
   public project = <any>[];
   public macroIssues = <any>[];
-
+  public totalTimeSpentSecs = 0;
+  public totalTimeSpent;
+  public timeEstimate;
 
   constructor(private apiService: ApiService,
     private route: ActivatedRoute) { Chart.register(...registerables) }
@@ -54,7 +56,7 @@ export class IssueDetailsComponent implements OnInit {
         (issue: Issue[]) => {
 
           this.issueList = issue;
-          console.log(this.issueList)
+          this.timeEstimate = this.time_convert(this.issueList.time_stats.time_estimate);
         }
       );
 
@@ -63,9 +65,36 @@ export class IssueDetailsComponent implements OnInit {
         (macro: Issue[]) => {
 
           this.macroIssues = macro;
-          console.log(this.macroIssues)
+
+          this.getTotalTimeSpent(this.macroIssues);
         }
       );
+  }
+
+
+  getTotalTimeSpent(macros) {
+
+    macros.forEach(element => {
+      this.totalTimeSpentSecs = this.totalTimeSpentSecs + element.time_stats.total_time_spent;
+    });
+
+    this.totalTimeSpent = this.time_convert(this.totalTimeSpentSecs);
+
+  }
+
+  time_convert(seconds) {
+    seconds = Number(seconds);
+    var d = Math.floor(seconds / (3600 * 24));
+    var h = Math.floor(seconds % (3600 * 24) / 3600);
+    var m = Math.floor(seconds % 3600 / 60);
+    var s = Math.floor(seconds % 60);
+
+    var dDisplay = d > 0 ? d + (d == 1 ? " day " : " days ") : "";
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour " : " hours ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    return dDisplay + hDisplay + mDisplay + sDisplay;
+
   }
 
   /* onCheckIssueDetails() {
