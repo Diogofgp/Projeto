9a9d48e8f0767901af7c97bba673f2fc709ce7ff
AndreFFranco <andrefranco@ipvc.pt>
2022-05-07 20:45:06 +0100

    active users listing

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -13,6 +13,7 @@ export class UsersComponent implements OnInit {
 
   sub: Subscription;
   public usersList = [];
+  public activeUsersList = [];
   loading$ = this.loader.loading$;
 
   constructor(private apiService: ApiService, public loader: LoadingService) { }
@@ -24,9 +25,27 @@ export class UsersComponent implements OnInit {
         (users: User[]) => {
           this.usersList = users;
           /*  console.log(projectList); */
+          this.getActiveUsers(this.usersList);
         }
       );
 
+  }
+
+  getActiveUsers(usersList) {
+    usersList.forEach(element => {
+      if (element.state == "active") {
+        this.activeUsersList.push(element);
+      }
+    });
+  }
+
+  onUserSelected() {
+
+    /* console.log(this.issue_item.iid);
+    /*  project_details/:id/issue_details/:issue_id */
+    /*  this.router.navigate([this.issue_item.iid, 'issue_details',]); */
+    //this.router.navigate(['project_details', this.issue_item.project_id, 'issue_details', this.issue_item.iid]);
+    // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route}); */
   }
 
 }
